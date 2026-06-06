3d74d7ada76ef005a6c0bc9c95811774f0b92afb
Danieh <148767868+daniiroblees@users.noreply.github.com>
2026-06-06 14:47:58 +0200

    Added distrito and municipio rest controllers

---
+++ src/main/java/com/leftjoiners/bancosol/proyectobackend/controller/rest/DistritoRestController.java
@@ -0,0 +1,25 @@
+package com.leftjoiners.bancosol.proyectobackend.controller.rest;
+
+import com.leftjoiners.bancosol.proyectobackend.dto.Distrito;
+import com.leftjoiners.bancosol.proyectobackend.service.DistritoService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@RequestMapping("/api/distritos")
+public class DistritoRestController {
+    private final DistritoService distritoService;
+
+    @GetMapping("/")
+    public List<Distrito> doInit() {
+        return this.distritoService.listarDistritos();
+    }
+
+}
---
+++ src/main/java/com/leftjoiners/bancosol/proyectobackend/controller/rest/MunicipioRestController.java
@@ -0,0 +1,24 @@
+package com.leftjoiners.bancosol.proyectobackend.controller.rest;
+
+import com.leftjoiners.bancosol.proyectobackend.dto.Municipio;
+import com.leftjoiners.bancosol.proyectobackend.service.MunicipioService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@RequestMapping("/api/municipios")
+public class MunicipioRestController {
+    private final MunicipioService municipioService;
+
+    @GetMapping("/")
+    public List<Municipio> doInit() {
+        return this.municipioService.listarMunicipios();
+    }
+}
